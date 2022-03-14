dbc32fec83d7e07cc4f52dc7e182a552cb3f1791
zakariamubarik <101276169+zakariamubarik@users.noreply.github.com>
2022-03-14 13:58:33 +0000

    upgreated file

--- TO DO APP/main.js
+++ TO DO APP/main.js
@@ -3,7 +3,7 @@ const todoInput = document.querySelector("#todo-input");
 const addBtn = document.querySelector("#add-btn");
 const todoList = document.querySelector("ul");
 
-let todoArray = new set();
+let todoArray = new Set();
 let user;
 
 //funtion to add new todo item 
@@ -14,10 +14,10 @@ if (todoArray.size != 5) {
         todoArray.add(todoInput.value);
         if (user) {
             let db = [...todoArray];
-            let_db = JSON.stringify(db);
+            let _db = (JSON.stringify(db)) ;
             localStorage.setItem(user, _db);
             setTodo(todoArray);
-
+ 
         } else {
             setTodo(todoArray);
         }
@@ -27,7 +27,7 @@ if (todoArray.size != 5) {
     }
     todoInput.value = "";
 } else {
-    alert("you have five thigs to do");
+    alert("you have five things to do");
  }
 };
 //event handlers
@@ -57,3 +57,49 @@ htmlArr += html;
  todoList.innerHTML = htmlArr;
 // informText()}
 }
+
+
+//sign in functions
+const signIn = document.querySelector(".logo-wrap p")
+const modal = document.querySelector("#modal")
+const userNm = document.querySelector("input")
+const signBtn = document.querySelector("button")
+const disModal = document.querySelector("span")
+
+signIn.addEventListener("click",() =>{
+    modal.style.display = "flex"
+})
+disModal.addEventListener("click",(e) =>{
+    modal.style.display = "none"
+})
+modal.addEventListener("click",(e) =>{
+    e.target == modal? modal.style.display = "none" : null
+})
+
+//funtion to log in user and save todo list
+signBtn.addEventListener("click", ()=>{
+    user = userNm.value
+    if(user ==""){
+        userNm.classList.add("error")
+    }else{
+        userNm.classList.remove("error")
+        signIn.innerText = user  //changing the "sign in" text to username
+        todoArray.clear()
+        setTodo(todoArray)
+        userNm.value = ""
+
+        let db = [...todoArray]
+        let _db =(JSON.stringify(db))
+        let userData = localStorage.getItem(user)
+
+        if(userData){
+            let _userData =JSON.parse(userData)
+            todoArray = new set(_userData)
+            setTodo(todoArray)
+            modal.style.display ="none"
+        }else{
+            localStorage.setItem(user, _db)
+            modal.style.display = "none"
+        }
+    }
+})
